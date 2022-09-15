607941aac7f322b7fdcd8749e184f4c2a09e44fe
Esteban Ramirez <netssrmrz@yahoo.com.au>
2022-09-15 15:20:08 +1000

    implement columns component

---
+++ public/components/De_Columns.js
@@ -0,0 +1,164 @@
+import Utils from "../lib/Utils.js";
+
+class De_Columns extends HTMLElement 
+{
+  static tname = "de-columns";
+
+  constructor() 
+  {
+    super();
+
+    this.edit_class = "edit_columns";
+    this.update_event = new Event('update');
+
+    Utils.Bind(this, "On_");
+  }
+
+  connectedCallback()
+  {
+    this.Render();
+  }
+
+  disconnectedCallback()
+  {
+
+  }
+
+  adoptedCallback()
+  {
+
+  }
+
+  //static observedAttributes = ["attr-name"];
+  attributeChangedCallback(attrName, oldValue, newValue)
+  {
+
+  }
+
+  get value()
+  {
+    let col_items = null;
+
+    if (this?.column_list?.children && this?.column_list?.children.length > 0)
+    {
+      col_items = [];
+      for (const col_item of this.column_list.children)
+      {
+        const id = col_item.getAttribute("col-id");
+        if (id)
+        {
+          const col_btn_id = "col_btn_" + id;
+          const visible = this[col_btn_id].checked;
+          const column = {id, visible};
+          col_items.push(column);
+        }
+      }
+    }
+
+    return col_items;
+  }
+
+  set value(col_items)
+  {
+    if (col_items && col_items.length > 0)
+    {
+      for (const column of col_items)
+      {
+        const col_btn_id = "col_btn_" + column.id;
+        this[col_btn_id].checked = column.visible;
+      }
+      this.dispatchEvent(this.update_event);
+    }
+  }
+
+  Save()
+  {
+    if (this.id)
+    {
+      const value_str = JSON.stringify(this.value);
+      localStorage.setItem(this.id, value_str);
+    }
+  }
+
+  Load()
+  {
+    if (this.id)
+    {
+      const value_str = localStorage.getItem(this.id);
+      this.value = JSON.parse(value_str);
+    }
+  }
+
+  // events =======================================================================================
+  
+  On_Click_Edit()
+  {
+    // show field menu
+    this.column_list.classList.add(this.edit_class);
+  }
+  
+  On_Click_Close()
+  {
+    // hide field menu
+    this.column_list.classList.remove(this.edit_class);
+  }
+  
+  On_Change_Column(e, sort)
+  {
+    // hide field menu
+    this.column_list.classList.remove(this.edit_class);
+
+    this.Save();
+    this.dispatchEvent(this.update_event);
+  }
+
+  // rendering ====================================================================================
+
+  Render()
+  {
+    const col_items = this.children;
+    for (const col_item of col_items)
+      this.Render_Column_Item(col_item);
+
+    const html = `
+      <div class="header">
+        Edit Columns: 
+        <span class="edit_banner">
+          <button id="edit_btn" class="btn edit_btn">✎</button>
+          <ul id="column_list" class="column_list">
+            <li class="hdr_item">
+              <button id="close_btn" class="btn close_btn">x</button> 
+              Columns</li>
+          </ul>
+        </span>
+      </div>
+    `;
+    const elems = Utils.toDocument(html);
+    elems.getElementById("column_list").append(...col_items);
+    this.append(elems);
+
+    Utils.Set_Id_Shortcuts(this, this);
+
+    this.edit_btn.addEventListener("click", this.On_Click_Edit);
+    this.close_btn.addEventListener("click", this.On_Click_Close);
+  }
+
+  Render_Column_Item(col_item)
+  {
+    const col_id = col_item.getAttribute("col-id");
+    const col_text = col_item.innerText;
+
+    const col_btn = document.createElement("input");
+    col_btn.id = "col_btn_" + col_id;
+    col_btn.type = "checkbox";
+    col_btn.checked = true;
+    col_btn.classList.add("btn");
+    col_btn.classList.add("col_btn");
+    col_btn.addEventListener("change", e => this.On_Change_Column(e));
+
+    col_item.classList.add("column_item");
+    col_item.append(col_btn);
+  }
+}
+
+export default De_Columns;
